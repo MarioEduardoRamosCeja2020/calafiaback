ecf4114b8fcae49c63c97face3eae2b88f28f0d5
Mario Eduardo Ramos Ceja <mario.eduardo.ramos.ceja@gmail.com>
2026-08-28 15:56:01 -0600

    fix certificado

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,26 @@
+// main.ts
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express = require('express');
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  
-  app.setGlobalPrefix('api');
+  const app = await NestFactory.create(AppModule);
+app.setGlobalPrefix('api');
 
   app.enableCors({
     origin: [
       'http://192.168.1.10:3000',
       'http://localhost:3000',
-      'http://www.grupocalafia.com.mx',
-      'https://www.grupocalafia.com.mx',
     ],
+
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+
     credentials: true,
   });
 
-  await app.init();
-  return server;
+  await app.listen(5000, '0.0.0.0');
+
+  console.log('🚀 Backend corriendo en http://0.0.0.0:5000');
 }
 
-module.exports = bootstrap();
+bootstrap();
